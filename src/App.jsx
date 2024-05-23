c39e1495eb8bca690d07f8d4738dc40c5eae376f
Gonzalezfelipe <felipegr.dev@gmail.com>
2024-05-23 11:34:48 -0400

    Finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,48 @@
-import './App.css'
-import Card from './Card'
-import Footer from './Footer'
-import Jumbotron from './Jumbotron'
-import NavBar from './NavBar'
-
+import './App.css';
+import Card from './Card';
+import Footer from './Footer';
+import Jumbotron from './Jumbotron';
+import NavBar from './NavBar';
 
 const cardData = [
   {
     id: 1,
-    title: 'Card 1',
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, consequuntur voluptatum corporis iusto impedit consectetur nesciunt nostrum harum repellendus aperiam exercitationem sapiente enim obcaecati, commodi, unde at maxime earum tempora?"
+    title: 'Card',
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
   },
   {
     id: 2,
     title: 'Card 2',
-    text: "Gain alignment future-proof agile, nor it's not hard guys, yet programmatically ramp up deliverables."
+    text: "Gain alignment future-proof agile, nor it's not hard guys."
   },
   {
     id: 3,
-    title: 'Card 3',
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, consequuntur voluptatum corporis iusto impedit consectetur nesciunt nostrum harum repellendus aperiam exercitationem sapiente enim obcaecati, commodi, unde at maxime earum tempora?"
+    title: 'Card',
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
   },
   {
     id: 4,
-    title: 'Card 4',
-    text: "Gain alignment future-proof agile, nor it's not hard guys, yet programmatically ramp up deliverables."
+    title: 'Card',
+    text: "Gain alignment future-proof agile, nor it's not hard guys."
   }
-]
+];
 
 function App() {
-
   return (
     <>
-    <NavBar/>
-    <Jumbotron/>
-    <Card/>
-    <Footer/>
+      <NavBar />
+      <Jumbotron />
+      <div className="container">
+        <div className="row">
+          {cardData.map(card => (
+            <Card key={card.id} title={card.title} text={card.text} />
+          ))}
+        </div>
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,29 +1,21 @@
-const Card = (id, title, text) => {
+const Card = ({ id, title, text }) => {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-12 col-md-6 col-lg-3 mb-5">
-          <div className="card" style={{ width: "19rem" }}>
-            <img src="https://via.placeholder.com/500x325" className="img-fluid" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title text-center">Card title</h5>
-              <p className="card-text text-center p-3">
-                Some quick example text to build on the card title and make up the bulk of the card's content.
-
-
-                ESTOY HACIENDO UNA PRUEBA
-              </p>
-            </div>
-            <ul className="list-group list-group-flush">
-              <li className="list-group-item text-center">
-                <button className="btn btn-primary d-inline-block">Find Out More!</button>
-              </li>
-            </ul>
-          </div>
+    <div className="col-12 col-md-6 col-lg-3 mb-5">
+      <div className="card" style={{ width: "19rem" }}>
+        <img src="https://via.placeholder.com/500x325" className="img-fluid" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title text-center">{title}</h5>
+          <p className="card-text text-center p-3">{text}</p>
         </div>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item text-center">
+            <button className="btn btn-primary d-inline-block">Find Out More!</button>
+          </li>
+        </ul>
       </div>
     </div>
   );
-}
+};
 
 export default Card;
+
